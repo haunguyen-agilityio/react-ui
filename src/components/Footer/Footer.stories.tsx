554db1191db7ffe2df0b80834f15d2cf1c206130
Huy Le Vu <huy.levu@asnet.com.vn>
2022-07-11 18:31:23 +0700

    chore: #21 Sync code from webapp to react-ui

---
+++ src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,30 @@
+// Libraries
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+// Components
+import Footer from '@components/Footer/index'
+
+export default {
+  title: 'Components/Footer',
+  component: Footer,
+} as ComponentMeta<typeof Footer>
+
+const Template: ComponentStory<typeof Footer> = (args) => <Footer {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  isDisabled: false,
+  isSelected: false,
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  isDisabled: true,
+  isSelected: false,
+}
+
+export const Selected = Template.bind({})
+Selected.args = {
+  isDisabled: false,
+  isSelected: true,
+}
