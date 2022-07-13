f73caec63f69024d330b3e98cdf208396417717b
Hau Nguyen <hau.nguyentrinh@asnet.com.vn>
2022-07-13 16:00:29 +0700

    tes

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,42 @@
 import resolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
 import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import dts from 'rollup-plugin-dts'
-
-const packageJson = require('./package.json')
+import del from 'rollup-plugin-delete'
 
 export default [
   {
     input: 'src/index.ts',
-    output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-        name: 'react-ui',
-      },
-      {
-        file: packageJson.module,
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
     plugins: [
-      external(),
-      resolve(),
+      del({ targets: 'dist/*' }),
+      external({ deps: true }),
+      resolve({
+        extensions: ['.js', '.ts', '.tsx'],
+      }),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
-      postcss(),
+      babel({
+        babelHelpers: 'runtime',
+        exclude: '**/node_modules/**',
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      }),
+      external(),
+      typescript(),
       terser(),
     ],
-  },
-  {
-    input: 'dist/esm/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    external: [/\.css$/],
-    plugins: [dts()],
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.es.js',
+        format: 'es',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
   },
 ]
